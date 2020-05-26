41ae354c1b7093d8d5bcbe5de472f3c7b4a19da2
Elonge11 <47829151+Elonge11@users.noreply.github.com>
2020-05-26 14:47:59 +0200

    faire un fetch à nouveau des produits depuis le serveur, récupérer les produits conservé dans sessionstore, les comparer avec les produits reçu par fetch, puis les afficher de manière dynamique dans la page produit

--- view/js/produit.js
+++ view/js/produit.js
@@ -1,65 +1,108 @@
-
-//ajouter l'évènement click sur tous les boutons sélectionner
-
-const elt = document.getElementsByClassName("select");
-
-function clicSelectButon () {
-    for (var i = 0 ; i < elt.length; i++) {
-        elt[i].addEventListener('click', putItem);
-     };
-}
-
-clicSelectButon();
-
-
-function putItem(e){
-    id = parseInt(e.target.getAttribute('data-id'))//renvoyer l'id en nombre entier
-    console.log('add to cart item', id);
-    getProducts();
-}
-
-putItem();
-
-var id;
-function getProducts (success, failure) {// requête du json data par fetch
-    fetch('https://raw.githubusercontent.com/ElongeR-12/projet_e_commerce.github.io/master/JSON/produts.json', {method: 'GET', mode:'cors'})
+let sessionStore = JSON.parse(sessionStorage.getItem('FFQFDQFQJYKOIUY9IEOPAZAR339209RHGBVfqkl'));
+console.log(sessionStore);
+let createProductsObj = () => {
+    fetch('http://localhost:3000/api/cameras', {method: 'GET', mode:'cors'})
     .then(function(response) {
         return response.json()
-    }).then(function(data) {
-        console.log('parsed json', data)
-        console.log(data[1].description)
-        
-            result = data.filter(obj => {
-                return obj.id === id;
-            })
-            console.log(result);
-            displayProduct();
+    }).then(function(products) {
+        console.log('parsed json', products);
+        displayProduct(products)
     })
     .catch(function(ex) {
         console.log('parsing failed', ex)
     })
 }
 
-var result;
-function displayProduct() { //afficher le produit dans la page produit
-    const elemt = document.getElementsByClassName('produit-desc-lense');
-    console.log(elemt, result);
-    let produitDescLense = `
-                                <div class="col-4 text-center border-bottom>
-                                <img src="${result[0].imageUrl}" class="img-fluid rounded img-thumbnail" alt="${result[0].name}">
-                                <h4>${result[0].name}</h4>
-                                </div>
-                                <div class="col-4 d-flex align-items-center justify-content-center border-bottom">
-                                    <p>${result[0].description}</p>
-                                </div>
-                                <div class="col-4 text-center border-bottom">
-                                    <img src="${result[0].imageUrl}" class="img-fluid rounded img-thumbnail" alt="${result[0].lenses[0]}">
-                                    <p>Lentilles</p>
-                                </div>
-                            
-                            `
-    console.log(produitDescLense);
-    elemt[0].innerHTML = produitDescLense;
-    console.log(elemt[0].innerHTML)
-}
+createProductsObj();
+let PRODUCTS = [];
+function displayProduct(products) {
+    PRODUCTS = products;
+    
+    let productsStored = [];
 
+    PRODUCTS.filter(product => {
+        sessionStore.forEach(store => {
+            if (product._id == store._id) {
+                productsStored.push(product) 
+            };
+        });
+    });
+    
+
+    let numberStored = productsStored.length;
+
+   
+    //cloner un bloc d'html
+    let noded = document.getElementsByClassName('produit-desc-lense');
+    for(let i=1; i<numberStored; i++){
+        
+        let clone = noded[0].cloneNode(true);
+        noded[0].after(clone);
+        
+    }
+
+    //source image des produits sélectionnés
+    let src = '';
+    let arraySrc = [];
+
+    for(const obj of productsStored){
+        src = obj.imageUrl;
+        arraySrc.push(src);       
+    }
+
+    
+    let img = document.getElementsByClassName('img-thumbnail');
+
+    let i=0;     
+    for(let element of arraySrc){
+       // ajouter le src dans image
+        img[i++].setAttribute('src', element)
+    }
+
+    //alt  image des produits sélectionnés
+    let alt = '';
+    let arrayAlt = [];
+
+    for(const obj of productsStored){
+        alt = obj.name;
+        arrayAlt.push(alt);       
+    }
+     
+    
+    for(let i=0; i<img.length; i++){
+        img[i].setAttribute('Alt', arrayAlt[i]);
+    }
+
+
+    // nom des produits sélectionnés
+    let name = '';
+    let arrayName = [];
+
+    for(const obj of productsStored){
+        name = obj.name;
+        arrayName.push(name);       
+    }
+
+    let h5 = document.querySelectorAll('h5');
+     
+    for(let i=0; i<h5.length; i++){
+        h5[i].textContent = arrayName[i] 
+    }
+
+     // description des produits sélectionnés
+     let description = '';
+     let arrayDescription = [];
+ 
+     for(const obj of productsStored){
+         description = obj.description;
+         arrayDescription.push(description);       
+     }
+ 
+     let textDescription = document.getElementsByClassName('description');
+      
+     for(let i=0; i<textDescription.length; i++){
+        textDescription[i].textContent = arrayDescription[i] 
+     }
+
+
+}
