d43d03fb4d05797acec59416fe7210deb33848de
Elonge11 <47829151+Elonge11@users.noreply.github.com>
2020-05-28 08:16:23 +0200

    programer l'affichage dynamique d'un seul produit dans la page produit

--- view/js/produit.js
+++ view/js/produit.js
@@ -14,47 +14,42 @@ let createProductsObj = () => {
 }
 
 createProductsObj();
-let PRODUCTS = [];
-function displayProduct(products) {
-    PRODUCTS = products;
-    
-    let productsStored = [];
 
-    PRODUCTS.filter(product => {
+function displayProduct(products) {
+        
+    const productsStored = [];//pour conserver les produits après vérification de son authenticité
+
+    products.filter(product => {//filtrer les produits qui correspond à un id dans session store
         sessionStore.forEach(store => {
             if (product._id == store._id) {
-                productsStored.push(product) 
+                productsStored.push(product)
             };
         });
     });
     
-
-    let numberStored = productsStored.length;
+    const numberStored = productsStored.length;
 
    
-    //cloner un bloc d'html
+    //cloner un bloc d'html// dans le cas où on vut ajouter plusieur produit dans la page
     let noded = document.getElementsByClassName('produit-desc-lense');
-    for(let i=1; i<numberStored; i++){
-        
-        let clone = noded[0].cloneNode(true);
-        noded[0].after(clone);
-        
+    for(let i=1; i<numberStored; i++){//itération en fonction du nombre de produit dans sessionstore
+        const clone = noded[0].cloneNode(true);
+        noded[0].after(clone); 
     }
 
     //source image des produits sélectionnés
-    let src = '';
-    let arraySrc = [];
+    let src = '';// stocker les différents src de chaque produit objet
+    let arraySrc = [];// assembler tous ses src dans un tableau
 
-    for(const obj of productsStored){
-        src = obj.imageUrl;
-        arraySrc.push(src);       
+    for(const obj of productsStored){// parcourir chaque élement objet dans tableau
+        src = obj.imageUrl;//assigner l'image url de chaque objet à src
+        arraySrc.push(src); // ajouter dans arraySrc après chaque assignation      
     }
 
-    
-    let img = document.getElementsByClassName('img-thumbnail');
+    const img = document.getElementsByClassName('img-thumbnail');// récupérer les éléments images avec classe img-thumbnai
 
-    let i=0;     
-    for(let element of arraySrc){
+    let i=0; //initier pour parcourir chaque image dans collection    
+    for(const element of arraySrc){
        // ajouter le src dans image
         img[i++].setAttribute('src', element)
     }
@@ -69,7 +64,7 @@ function displayProduct(products) {
     }
      
     
-    for(let i=0; i<img.length; i++){
+    for(let i=0; i<img.length; i++){//parcourir l'image et ajouter l'attribut alt pour chaque
         img[i].setAttribute('Alt', arrayAlt[i]);
     }
 
@@ -83,7 +78,7 @@ function displayProduct(products) {
         arrayName.push(name);       
     }
 
-    let h5 = document.querySelectorAll('h5');
+    const h5 = document.querySelectorAll('h5');
      
     for(let i=0; i<h5.length; i++){
         h5[i].textContent = arrayName[i] 
@@ -98,11 +93,35 @@ function displayProduct(products) {
          arrayDescription.push(description);       
      }
  
-     let textDescription = document.getElementsByClassName('description');
+     const textDescription = document.getElementsByClassName('description');
       
      for(let i=0; i<textDescription.length; i++){
         textDescription[i].textContent = arrayDescription[i] 
      }
 
+    // les lentilles des produits sélectionnés
+
+    let lense = '';
+    let arrayLenses = [];
+ 
+    for(const obj of productsStored){
+         lense = obj.lenses;
+         arrayLenses.push(lense);       
+     }
+
+    //récupérer l'élement parent select des options
+    function addSelect() {
+
+        const parentSelect = document.getElementsByClassName('parentSelect');
+        const select = document.createElement("select");
+        select.setAttribute("id","select");
+        parentSelect[0].append(select);
+        select.add(new Option('Choisissez une lentille'));
+        for(i=0; i<arrayLenses[0].length; i++){
+            select.add(new Option(arrayLenses[0][i],arrayLenses[0][i]))
+        }
+        console.log(parentSelect[0]);
+    }
+    addSelect();
 
 }
