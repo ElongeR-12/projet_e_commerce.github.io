d7703bd57837673a583c4e8323f8a31972271187
Elonge11 <47829151+Elonge11@users.noreply.github.com>
2020-06-01 09:23:02 +0200

    programmer de manière dynamique l'affichage des produits ajouté dans panier

--- view/js/panier.js
+++ view/js/panier.js
@@ -1,1 +1,14 @@
-console.log('connecte on panier');
+'use strict'
+
+function displayResume() {
+    const LOCALSTORAGE = JSON.parse(localStorage.getItem('LOCALSTORE'));
+    console.log(LOCALSTORAGE);
+
+    let toClone = document.getElementsByClassName('resume-area');//récupérer l'élement html
+    for(let i=1; i<LOCALSTORAGE.length; i++){	    
+        let clone = toClone[0].cloneNode(true);//cloner l'élément et ses nodes      
+        toClone[0].after(clone); //cloner l'élement clone après l'original
+    }
+}
+
+displayResume();
