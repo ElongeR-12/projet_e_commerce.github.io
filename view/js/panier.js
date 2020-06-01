10ac7b8b783af51967c8b81e461aa0786c77b0ae
Elonge11 <47829151+Elonge11@users.noreply.github.com>
2020-06-01 13:22:07 +0200

    ajouter de manière dynamique les produits dans panier

--- view/js/panier.js
+++ view/js/panier.js
@@ -9,6 +9,84 @@ function displayResume() {
         let clone = toClone[0].cloneNode(true);//cloner l'élément et ses nodes      
         toClone[0].after(clone); //cloner l'élement clone après l'original
     }
-}
+
+    const NAME = document.querySelectorAll('h3')
+    console.log(NAME);
+    let i=0;
+    for (const element of LOCALSTORAGE){
+        NAME[i++].textContent = element.name;
+    }
+
+    const IMG = document.getElementsByClassName('image-to-resume');
+    console.log(IMG);
+    let j=0;
+    let k=0;
+    for(const element of LOCALSTORAGE){
+        IMG[j++].setAttribute("src", element.imageUrl);
+        IMG[k++].setAttribute("alt", element.name)
+    }
+
+    const DESCRIPTION = document.getElementsByClassName('description');
+    let l=0;
+    for(const element of LOCALSTORAGE){
+        DESCRIPTION[l++].textContent = element.description
+    }
+
+    const PRICE = document.getElementsByClassName('price');
+    let m=0;
+    for(const element of LOCALSTORAGE){
+        PRICE[m++].textContent ='EUR  '+ element.price;
+    }
+
+    const TOTALPRICE = document.getElementsByClassName('total-price');
+    let n=0;
+    for(const element of LOCALSTORAGE){
+        TOTALPRICE[n++].textContent = 'Sous-total des articles :';
+    }
+
+    const TOTALPRICEVALUE = document.getElementsByClassName('total-price-value');
+    let o=0;
+    for(const element of LOCALSTORAGE){
+        let total = (element.price * element.quantity);      
+        TOTALPRICEVALUE[o++].textContent = 'EUR  '+ total;
+    }
+
+    const  EXPEDITION = document.getElementsByClassName('expedition-price');
+    let p=0;
+    for(const element of LOCALSTORAGE){      
+        EXPEDITION[p++].textContent = "Frais d'envoi :";
+    }
+
+    const  EXPEDITIONVALUE= document.getElementsByClassName('expedition-price-value');
+    let q=0;
+    for(const element of LOCALSTORAGE){      
+        EXPEDITIONVALUE[q++].textContent = "EUR 0,00";
+    }
+
+    const  QUANTITY= document.getElementsByClassName('product-quantity');
+    let r=0;
+    for(const element of LOCALSTORAGE){      
+        QUANTITY[r++].textContent = "Nombre d'articles :";
+    }
+
+    const  QUANTITYVALUE= document.getElementsByClassName('product-quantity-value');
+    let s=0;
+    for(const element of LOCALSTORAGE){      
+        QUANTITYVALUE[s++].textContent = element.quantity;
+    }
+
+    const  LENSE = document.getElementsByClassName('lenses');
+    let t=0;
+    for(const element of LOCALSTORAGE){      
+        LENSE[t++].textContent = "Catégorie lentille :";
+    }
+
+    const  LENSEVALUE= document.getElementsByClassName('lenses-value');
+    let u=0;
+    for(const element of LOCALSTORAGE){      
+        LENSEVALUE[u++].textContent = element.lenses;
+    }
+
+};
 
 displayResume();
