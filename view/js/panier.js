c259d49986ba3af00226d1c2d45a3f79dd3e8f9c
Elonge11 <47829151+Elonge11@users.noreply.github.com>
2020-06-03 22:07:30 +0200

    gérer post fetch, à débuger, bad request err400, reponse vide

--- view/js/panier.js
+++ view/js/panier.js
@@ -4,10 +4,10 @@ function displayResume() {
     const LOCALSTORAGE = JSON.parse(localStorage.getItem('LOCALSTORE'));
     console.log(LOCALSTORAGE);
 
-    
-    let toClone = document.getElementsByClassName('resume-area');//récupérer l'élement html
-    for(let i=0; i<LOCALSTORAGE.length; i++){	    
-        let clone = toClone[0].cloneNode(true);//cloner l'élément et ses nodes      
+
+    let toClone = document.getElementsByClassName('resume-area'); //récupérer l'élement html
+    for (let i = 0; i < LOCALSTORAGE.length; i++) {
+        let clone = toClone[0].cloneNode(true); //cloner l'élément et ses nodes      
         toClone[0].after(clone); //cloner l'élement clone après l'original
         const BORDER = document.getElementsByClassName("add-border");
         BORDER[0].classList.add('border');
@@ -18,82 +18,80 @@ function displayResume() {
 
     const NAME = document.querySelectorAll('h3')
     console.log(NAME);
-    let i=0;
-    for (const element of LOCALSTORAGE){
+    let i = 0;
+    for (const element of LOCALSTORAGE) {
         NAME[i++].textContent = element.name;
     }
 
     const IMG = document.getElementsByClassName('image-to-resume');
     console.log(IMG);
-    let j=0;
-    let k=0;
-    for(const element of LOCALSTORAGE){
+    let j = 0;
+    let k = 0;
+    for (const element of LOCALSTORAGE) {
         IMG[j++].setAttribute("src", element.imageUrl);
         IMG[k++].setAttribute("alt", element.name)
     }
 
     const DESCRIPTION = document.getElementsByClassName('description');
-    let l=0;
-    for(const element of LOCALSTORAGE){
+    let l = 0;
+    for (const element of LOCALSTORAGE) {
         DESCRIPTION[l++].textContent = element.description
     }
 
     const PRICE = document.getElementsByClassName('price');
-    let m=0;
-    for(const element of LOCALSTORAGE){
-        PRICE[m++].textContent ='EUR  '+ element.price;
+    let m = 0;
+    for (const element of LOCALSTORAGE) {
+        PRICE[m++].textContent = 'EUR  ' + element.price;
     }
 
     const TOTALPRICE = document.getElementsByClassName('total-price');
-    let n=0;
-    for(const element of LOCALSTORAGE){
+    let n = 0;
+    for (const element of LOCALSTORAGE) {
         TOTALPRICE[n++].textContent = 'Sous-total des articles :';
     }
 
     const TOTALPRICEVALUE = document.getElementsByClassName('total-price-value');
-    let o=0;
-    for(const element of LOCALSTORAGE){
-        let total = (element.price * element.quantity);      
-        TOTALPRICEVALUE[o++].textContent = 'EUR  '+ total;
+    let o = 0;
+    for (const element of LOCALSTORAGE) {
+        let total = (element.price * element.quantity);
+        TOTALPRICEVALUE[o++].textContent = 'EUR  ' + total;
     }
 
-    const  EXPEDITION = document.getElementsByClassName('expedition-price');
-    let p=0;
-    for(const element of LOCALSTORAGE){      
+    const EXPEDITION = document.getElementsByClassName('expedition-price');
+    let p = 0;
+    for (const element of LOCALSTORAGE) {
         EXPEDITION[p++].textContent = "Frais d'envoi :";
     }
 
-    const  EXPEDITIONVALUE= document.getElementsByClassName('expedition-price-value');
-    let q=0;
-    for(const element of LOCALSTORAGE){      
+    const EXPEDITIONVALUE = document.getElementsByClassName('expedition-price-value');
+    let q = 0;
+    for (const element of LOCALSTORAGE) {
         EXPEDITIONVALUE[q++].textContent = "EUR 0,00";
     }
 
-    const  QUANTITY= document.getElementsByClassName('product-quantity');
-    let r=0;
-    for(const element of LOCALSTORAGE){      
+    const QUANTITY = document.getElementsByClassName('product-quantity');
+    let r = 0;
+    for (const element of LOCALSTORAGE) {
         QUANTITY[r++].textContent = "Nombre d'articles :";
     }
 
-    const  QUANTITYVALUE= document.getElementsByClassName('product-quantity-value');
-    let s=0;
-    for(const element of LOCALSTORAGE){      
+    const QUANTITYVALUE = document.getElementsByClassName('product-quantity-value');
+    let s = 0;
+    for (const element of LOCALSTORAGE) {
         QUANTITYVALUE[s++].textContent = element.quantity;
     }
 
-    const  LENSE = document.getElementsByClassName('lenses');
-    let t=0;
-    for(const element of LOCALSTORAGE){      
+    const LENSE = document.getElementsByClassName('lenses');
+    let t = 0;
+    for (const element of LOCALSTORAGE) {
         LENSE[t++].textContent = "Catégorie lentille :";
     }
 
-    const  LENSEVALUE= document.getElementsByClassName('lenses-value');
-    let u=0;
-    for(const element of LOCALSTORAGE){      
+    const LENSEVALUE = document.getElementsByClassName('lenses-value');
+    let u = 0;
+    for (const element of LOCALSTORAGE) {
         LENSEVALUE[u++].textContent = element.lenses;
     }
-
-    
 
     const H5 = document.querySelectorAll('h5');
     H5[0].textContent = 'Total';
@@ -106,15 +104,14 @@ function displayResume() {
 
     const TVA = document.getElementById('tva');
     TVA.textContent = 'Total (TVA incluse)';
-    
+
     const ARRAY = [];
     let produce;
-    LOCALSTORAGE.map(item =>{
+    LOCALSTORAGE.map(item => {
         produce = item.price * item.quantity;
         ARRAY.push(produce);
     })
     console.log(ARRAY);
-    
 
     const reducer = (accumulator, currentValue) => accumulator + currentValue;
     const TOTALBOOK = ARRAY.reduce(reducer);
@@ -124,17 +121,69 @@ function displayResume() {
     const FINALEXPEDITION = document.getElementById('final-expetition');
     FINALEXPEDITION.textContent = "gratuit";
     const TOTALWITHTVA = document.getElementById('total-with-tva');
-    TOTALWITHTVA.textContent= TOTALBOOK + ' €';
+    TOTALWITHTVA.textContent = TOTALBOOK + ' €';
     const borderBottom = document.getElementById("add-border-bottom");
     borderBottom.classList.add('border-bottom');
     const BOOKING = document.getElementsByClassName('bg-warning');
     BOOKING[0].classList.remove('d-none');
     BOOKING[0].addEventListener('click', showForm);
 
-    function showForm(ev){
+    function showForm(ev) {
         ev.preventDefault();
         const FORM = document.getElementById('contactform-form');
         FORM.classList.remove("d-none");
+    }
+
+    ///faire post fetch
+    document.getElementById('contactform-form').addEventListener('submit', postBooking);
+
+    function postBooking(e) {
+        e.preventDefault();
+
+        let firstName = document.getElementById('firstName').value;
+        console.log(firstName);
+        let lastName = document.getElementById('lastName').value;
+        let address = document.getElementById('address').value;
+        let city = document.getElementById('city').value;
+        let email = document.getElementById('email').value;
+
+        let contact = {
+            firstName: firstName,
+            lastName: lastName,
+            address: address,
+            city: city,
+            email: email
+        };
+        let productsResume = LOCALSTORAGE;
+        let products = [];
+        productsResume.forEach(element => {
+            products.push(element._id);
+        });
+        console.log(products);
+
+        fetch('http://localhost:3000/api/cameras/order', {
+                method: 'post',
+                headers: {
+                    "Content-type": "application/json"
+                },
+                body: JSON.stringify({
+                    firstName: contact.firstName,
+                    lastName: contact.lastName,
+                    address: contact.address,
+                    city: contact.city,
+                    email: contact.email,
+                    products
+                })
+            })
+            .then(function (response) {
+                return response.json()
+            })
+            .then(function (data) {
+                console.log('Request succeeded with JSON response', data);
+            })
+            .catch(function (error) {
+                console.log('Request failed', error);
+            });
     }
 };
 
