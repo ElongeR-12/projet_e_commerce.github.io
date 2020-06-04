d4d53fce75c995ad7b753d48cbf0f38d597f3b03
Elonge11 <47829151+Elonge11@users.noreply.github.com>
2020-06-04 12:28:41 +0200

    gérer la page remerciement

--- view/js/remerciement.js
+++ view/js/remerciement.js
@@ -1,1 +1,18 @@
-console.log("conncted on remerciement");
+const ORDERID = document.getElementsByClassName('orderId');
+const LOCALORDERID = JSON.parse(localStorage.getItem('orderId'));
+const COST = JSON.parse(localStorage.getItem('finalPrice'));
+console.log(LOCALORDERID);
+
+ORDERID[0].textContent = 'commande n° ' + LOCALORDERID;
+
+const GREETING = document.getElementsByClassName('bonjour');
+GREETING[0].textContent = 'Bonjour,';
+const FIRSTTEXT = document.getElementsByClassName('text1');
+FIRSTTEXT[0].textContent = 'Nous vous remercions de votre commande. Nous vous tiendrons informés par e-mail lorsque les articles de votre commande auront été expédiés. '
+const SECONDTEXT = document.getElementsByClassName('text2');
+SECONDTEXT[0].textContent = 'Nous rappelons que le montant de votre commande est de '+ COST + " euros sous l'identifiant n°" + LOCALORDERID +'.';
+
+const WISH = document.getElementsByClassName("wish")[0];
+WISH.textContent = 'Nous espérons vous revoir bientôt.';
+const SIGN = document.getElementsByClassName("sign")[0];
+SIGN.textContent = "Ornico.fr";
