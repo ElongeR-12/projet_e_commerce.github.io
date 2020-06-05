1202aff2fbed374dcc8f8af677cadc93419fdf51
Elonge11 <47829151+Elonge11@users.noreply.github.com>
2020-06-05 10:38:48 +0200

    reorganiser les codes en fonction

--- view/js/remerciement.js
+++ view/js/remerciement.js
@@ -1,18 +1,20 @@
-const ORDERID = document.getElementsByClassName('orderId');
-const LOCALORDERID = JSON.parse(localStorage.getItem('orderId'));
-const COST = JSON.parse(localStorage.getItem('finalPrice'));
-console.log(LOCALORDERID);
+(function displayThanking() {
+    const ORDERID = document.getElementsByClassName('orderId');
+    const localOrderId = JSON.parse(localStorage.getItem('orderId'));
+    const cost = JSON.parse(localStorage.getItem('finalPrice'));
+    console.log(localOrderId);
 
-ORDERID[0].textContent = 'commande n° ' + LOCALORDERID;
+    ORDERID[0].textContent = 'commande n° ' + localOrderId;
 
-const GREETING = document.getElementsByClassName('bonjour');
-GREETING[0].textContent = 'Bonjour,';
-const FIRSTTEXT = document.getElementsByClassName('text1');
-FIRSTTEXT[0].textContent = 'Nous vous remercions de votre commande. Nous vous tiendrons informés par e-mail lorsque les articles de votre commande auront été expédiés. '
-const SECONDTEXT = document.getElementsByClassName('text2');
-SECONDTEXT[0].textContent = 'Nous rappelons que le montant de votre commande est de '+ COST + " euros sous l'identifiant n°" + LOCALORDERID +'.';
+    const GREETING = document.getElementsByClassName('bonjour');
+    GREETING[0].textContent = 'Bonjour,';
+    const FIRSTTEXT = document.getElementsByClassName('text1');
+    FIRSTTEXT[0].textContent = 'Nous vous remercions de votre commande. Nous vous tiendrons informés par e-mail lorsque les articles de votre commande auront été expédiés. '
+    const SECONDTEXT = document.getElementsByClassName('text2');
+    SECONDTEXT[0].textContent = 'Nous rappelons que le montant de votre commande est de ' + cost + " euros sous l'identifiant n°" + localOrderId + '.';
 
-const WISH = document.getElementsByClassName("wish")[0];
-WISH.textContent = 'Nous espérons vous revoir bientôt.';
-const SIGN = document.getElementsByClassName("sign")[0];
-SIGN.textContent = "Ornico.fr";
+    const WISH = document.getElementsByClassName("wish")[0];
+    WISH.textContent = 'Nous espérons vous revoir bientôt.';
+    const SIGN = document.getElementsByClassName("sign")[0];
+    SIGN.textContent = "Ornico.fr";
+})();
